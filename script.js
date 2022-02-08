2abf9936dc2f1b7a7564ca596d3cb837a50b4862
omkar <omkarambi44@gmail.com>
2022-02-08 14:38:03 +0530

    js

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateemail(email)
+{
+    var patten = new RegExp(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+    return patten.test(email);
+}
+
+function validatemobileno(mobileno)
+{
+    var patten = new RegExp(/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/);
+    return patten.test(mobileno);
+    
+}
